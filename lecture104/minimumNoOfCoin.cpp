3fe052cc8b1d1bcfb8e38309d6455dd87417aaa5
vicky <try.vickyjha@gmail.com>
2026-01-31 10:00:44 +0530

    minimum No of coin

--- lecture104/minimumNoOfCoin.cpp
+++ lecture104/minimumNoOfCoin.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 #include<climits>
 using namespace std;
 
@@ -15,7 +16,39 @@ using namespace std;
     return mini;
 }
 
+// using DP - tabulation
+int minCoinsDP(vector<int> &coins, int x){
+    vector<int> dp(x+1, INT_MAX);
+    dp[0] = 0;
+    for(int i = 0;i<=x;i++){
+        for(int j = 0;j<coins.size();j++){
+            if(i - coins[j] >= 0 && dp[i - coins[j]] != INT_MAX){
+                dp[i] = min(dp[i], 1+ dp[i-coins[j]]);
+            }
+        }
+    }
+    
+    if(dp[x] == INT_MAX) return -1;
+    return dp[x];
+}
+
+int solve(vector<int> &coins, int x){
+    vector<int> dp(x+1, INT_MAX);
+    dp[0] = 0;
+    for(int i = 0;i<=x;i++){
+        for(int coin : coins){
+            if(i- coin >= 0 && dp[i - coin] != INT_MAX){
+                dp[i] = min(dp[i], 1 + dp[i- coin]);
+            }
+        }
+    }
+    return dp[x] == INT_MAX ? -1 : dp[x];
+}
+
 
  int main(){
-
+    vector<int> coins = {1,3,4};
+    int x = 6;
+    cout<<minCoinsDP(coins,x)<<endl;
+    cout<<solve(coins,x)<<endl;
  }
