e38b1698704a2bee97ba890d816a7a43cfa8c681
VICKYJHA <try.vickyjha@gmail.com>
2026-04-19 17:04:06 +0530

    lis using tabulation

--- z.dp/lecture119/lis.cpp
+++ z.dp/lecture119/lis.cpp
@@ -30,8 +30,31 @@ int lengthOfLIS(vector<int>& nums) {
     return solve(nums, 0, -1, dp);
 }
 
+
+int tabulation(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+    for(int index = n - 1; index >= 0; index--) {
+        for(int prev = index - 1; prev >= -1; prev--) {
+
+            int notTake = dp[index + 1][prev + 1];
+
+            int take = 0;
+            if(prev == -1 || nums[index] > nums[prev]) {
+                take = 1 + dp[index + 1][index + 1];
+            }
+
+            dp[index][prev + 1] = max(take, notTake);
+        }
+    }
+    return dp[0][0];
+}
+
+
 int main() {
     vector<int> nums = {10,9,2,5,3,7,101,18};
     cout << lengthOfLIS(nums) << endl;  // Output: 4
+    cout <<tabulation(nums) << endl;  // Output: 4
     return 0;
 }
