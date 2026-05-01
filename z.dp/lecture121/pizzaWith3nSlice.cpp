ee1b60aed679d519ee768ec957a384d6a1fddcfb
VICKYJHA <try.vickyjha@gmail.com>
2026-05-01 21:43:14 +0530

    pizza with 3n slices from tabulation

--- z.dp/lecture121/pizzaWith3nSlice.cpp
+++ z.dp/lecture121/pizzaWith3nSlice.cpp
@@ -26,11 +26,44 @@ int maxSizeSlices(vector<int>& slices) {
     return max(case1,case2);
 }
 
+int maxSizeSlicesTab(vector<int>& slices) {
+    int k = slices.size();
+    vector<vector<int>> dp(k+2,vector<int>(k+2,0));
+    vector<vector<int>> dp2(k+2,vector<int>(k+2,0));
+
+    for(int i = k-2;i>=0;i--){
+        for(int n = 1;n<=k/3;n++){
+            int take = slices[i]+ dp[i+2][n-1];
+            int nottake =  dp[i+1][n];
+
+            dp[i][n] = max(take,nottake);
+        }
+    }
+
+    int case1 = dp[0][k/3];
+
+    for(int i = k-1;i>=1;i--){
+        for(int n = 1;n<=k/3;n++){
+            int take = slices[i]+ dp2[i+2][n-1];
+            int nottake =  dp2[i+1][n];
+
+            dp2[i][n] = max(take,nottake);
+        }
+    }
+
+    int case2 = dp2[1][k/3];
+
+    return max(case1,case2);
+
+}
+
 int main(){
     vector<int> slices = {1, 2, 3, 4, 5, 6};
     int memorizationAns =  maxSizeSlices(slices);
+    int tabultationAns = maxSizeSlicesTab(slices);
 
     cout<<"The ans from the Memorization is : "<<memorizationAns<<endl;
+    cout<<"The ans from the Tabulation is : "<<tabultationAns<<endl;
     return 0;
 
 }
