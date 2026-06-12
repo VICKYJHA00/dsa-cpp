d9527d24282f01dd72a03a3696fffe7bfb677edd
VICKYJHA00 <try.vickyjha@gmail.com>
2026-06-12 13:01:18 +0530

    the minimum operation to match two string with three operation like insert delete  and replace usign memeoization and tabulation

--- z.dp/lecture137/editDistance.cpp
+++ z.dp/lecture137/editDistance.cpp
@@ -27,11 +27,44 @@ public:
         return solve(0,0,word1,word2,dp);
     }
 };
+class tabulation {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.length()+1,vector<int>(word2.length()+1,0));
+        int n = word1.length();
+        int m = word2.length(); 
+        for(int j = 0;j<=m;j++){
+            dp[n][j] = m-j;
+        }
+        for(int i = 0;i<=n;i++){
+            dp[i][m] = n-i;
+        }
+        for(int i = n-1;i>= 0;i--){
+            for(int j = m-1;j>= 0;j--){
+                int len = 0;
+                if(word1[i] == word2[j]){
+                    len = dp[i+1][j+1];
+                }else{
+                    int delet = 1 + dp[i][j+1];
+                    int insert = 1 + dp[i+1][j];
+                    int replace = 1 + dp[i+1][j+1];
+
+                    len = min(delet,min(insert,replace));
+                }
+                dp[i][j] = len;
+            }
+        }
+        return dp[0][0];
+    }
+};
 
 int main() {
     string word1 = "intention";
     string word2 = "execution";
     Solution sol;
+    tabulation t;
     cout<<"The minimum operations to match the string is :"<<sol.minDistance(word1,word2);
+    cout<<endl;
+    cout<<"The minimum operations to match the string is using tabulation :"<<t.minDistance(word1,word2);
     return 0;
 }
