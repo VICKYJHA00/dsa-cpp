a2d7d061c4e77bedc61bd009e9b0f9b1e23b6459
VICKYJHA00 <try.vickyjha@gmail.com>
2026-06-14 21:46:30 +0530

    The wildcard pattern by tabulation

--- z.dp/lecture139/wildCardPattern.cpp
+++ z.dp/lecture139/wildCardPattern.cpp
@@ -3,27 +3,55 @@ using namespace std;
 
 class Solution {
 public:
-    bool solve(string& s, string& p, int i ,int j , vector<vector<int>>& dp){
-        if(i<0 && j<0) return true;
-        if(i>=0 && j<0) return false;
-        if(i<0 && j>=0){
-            for(int k = 0;k<=j;k++ ){
-                if(p[k] != '*') return false;
+    // bool solve(string& s, string& p, int i ,int j , vector<vector<int>>& dp){
+    //     if(i==0 && j==0) return true;
+    //     if(i>0 && j==0) return false; 
+    //     if(i==0 && j>0){
+    //         for(int k = 0;k<j;k++ ){
+    //             if(p[k] != '*') return false;
+    //         }
+    //         return true;
+    //     } 
+        
+    //     if(dp[i][j] != -1) return dp[i][j];
+
+    //     if(s[i-1] == p[j-1] || p[j-1] == '?') return dp[i][j] = solve(s,p,i-1,j-1,dp);
+    //     else if(p[j-1] == '*')  return dp[i][j] = solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp);
+    //     else return dp[i][j] =  false;
+    // }
+    bool solveTab(string s, string p) {
+        int m = s.length();
+        int n = p.length();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        dp[0][0] = true;
+        for(int j = 1;j<=n;j++){
+            bool flag = true;
+            for(int k = 0;k<j;k++ ){
+                if(p[k] != '*'){
+                    flag = false;
+                    break;
+                }
             }
-            return true;
+            dp[0][j] = flag;
         }
 
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == p[j] || p[j] == '?') return dp[i][j] = solve(s,p,i-1,j-1,dp);
-        else if(p[j] == '*')  return dp[i][j] = solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp);
-        else return false;
+        for(int i =1;i<=m;i++){
+            for(int j = 1;j<=n;j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?')  dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*')   dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                else  dp[i][j] =  false;
+            }
+        }
+
+        return dp[m][n];
     }
 
     bool isMatch(string s, string p) {
         int i = s.length();
         int j = p.length();
         vector<vector<int>>dp(i+1,vector<int>(j+1,-1));
-        return solve(s,p,i-1,j-1,dp);
+        // return solve(s,p,i,j,dp);
+        return solveTab(s,p);
     }
 };
 
@@ -36,5 +64,5 @@ int main() {
     }else{
         cout<<"The answer is  FALSE";
     }
-    return 0;
+    return 0;wildcard
 }
