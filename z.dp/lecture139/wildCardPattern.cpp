f55115671b44777e2754a51ce7be256093531f77
VICKYJHA00 <try.vickyjha@gmail.com>
2026-06-14 22:00:34 +0530

    The wildcard pattern by Space Optimized

--- z.dp/lecture139/wildCardPattern.cpp
+++ z.dp/lecture139/wildCardPattern.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 class Solution {
 public:
+
+
     // bool solve(string& s, string& p, int i ,int j , vector<vector<int>>& dp){
     //     if(i==0 && j==0) return true;
     //     if(i>0 && j==0) return false; 
@@ -19,11 +21,45 @@ public:
     //     else if(p[j-1] == '*')  return dp[i][j] = solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp);
     //     else return dp[i][j] =  false;
     // }
-    bool solveTab(string s, string p) {
+
+
+
+    // bool solveTab(string s, string p) {
+    //     int m = s.length();
+    //     int n = p.length();
+    //     vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+    //     dp[0][0] = true;
+    //     for(int j = 1;j<=n;j++){
+    //         bool flag = true;
+    //         for(int k = 0;k<j;k++ ){
+    //             if(p[k] != '*'){
+    //                 flag = false;
+    //                 break;
+    //             }
+    //         }
+    //         dp[0][j] = flag;
+    //     }
+
+    //     for(int i =1;i<=m;i++){
+    //         for(int j = 1;j<=n;j++){
+    //             if(s[i-1] == p[j-1] || p[j-1] == '?')  dp[i][j] = dp[i-1][j-1];
+    //             else if(p[j-1] == '*')   dp[i][j] = dp[i][j-1] || dp[i-1][j];
+    //             else  dp[i][j] =  false;
+    //         }
+    //     }
+
+    //     return dp[m][n];
+    // }
+
+
+
+       bool solveOP(string s, string p) {
         int m = s.length();
         int n = p.length();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-        dp[0][0] = true;
+        // vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        vector<int>curr(n+1,0);
+        vector<int>prev(n+1,0);
+        prev[0] = true;
         for(int j = 1;j<=n;j++){
             bool flag = true;
             for(int k = 0;k<j;k++ ){
@@ -32,26 +68,31 @@ public:
                     break;
                 }
             }
-            dp[0][j] = flag;
+            prev[j] = flag;
         }
 
         for(int i =1;i<=m;i++){
+            fill(curr.begin(), curr.end(), false);
+            curr[0] = false;
             for(int j = 1;j<=n;j++){
-                if(s[i-1] == p[j-1] || p[j-1] == '?')  dp[i][j] = dp[i-1][j-1];
-                else if(p[j-1] == '*')   dp[i][j] = dp[i][j-1] || dp[i-1][j];
-                else  dp[i][j] =  false;
+                if(s[i-1] == p[j-1] || p[j-1] == '?')  curr[j] = prev[j-1];
+                else if(p[j-1] == '*')   curr[j] = curr[j-1] || prev[j];
+                else  curr[j] =  false;
             }
+            prev = curr;
         }
 
-        return dp[m][n];
+        return prev[n];
     }
+
+
 
     bool isMatch(string s, string p) {
         int i = s.length();
         int j = p.length();
         vector<vector<int>>dp(i+1,vector<int>(j+1,-1));
         // return solve(s,p,i,j,dp);
-        return solveTab(s,p);
+        return solveOP(s,p);
     }
 };
 
@@ -64,5 +105,5 @@ int main() {
     }else{
         cout<<"The answer is  FALSE";
     }
-    return 0;wildcard
+    return 0;
 }
