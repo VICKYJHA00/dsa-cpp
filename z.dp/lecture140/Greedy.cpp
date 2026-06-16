c5df1d14099fdaaa43b1a3e74b884de8edb17142
VICKYJHA00 <try.vickyjha@gmail.com>
2026-06-16 12:01:03 +0530

    greedy

--- z.dp/lecture140/Greedy.cpp
+++ z.dp/lecture140/Greedy.cpp
@@ -26,6 +26,38 @@ class activitySelection {
     }
 };
 
+class minMax {
+public:
+    vector<int> minMaxCandy(vector<int>& prices, int k) {
+        sort(prices.begin(), prices.end());
+
+        int n = prices.size();
+
+        int minCost = 0, maxCost = 0;
+
+        int i = 0, j = n - 1;
+
+        // Minimum cost
+        while (i <= j) {
+            minCost += prices[i];
+            i++;
+            j -= k;
+        }
+
+        i = 0;
+        j = n - 1;
+
+        // Maximum cost
+        while (i <= j) {
+            maxCost += prices[j];
+            j--;
+            i += k;
+        }
+
+        return {minCost, maxCost};
+    }
+};
+
 
 int main() {
     
