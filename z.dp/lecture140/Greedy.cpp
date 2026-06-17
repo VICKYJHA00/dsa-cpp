50f9c1d08b146de476f3dec3a74e8eafe90d5ad5
VICKYJHA00 <try.vickyjha@gmail.com>
2026-06-17 14:14:28 +0530

    greedy

--- z.dp/lecture140/Greedy.cpp
+++ z.dp/lecture140/Greedy.cpp
@@ -58,6 +58,105 @@ public:
     }
 };
 
+class survivalDays {
+  public:
+    int minimumDays(int S, int N, int M) {
+        // code here
+        if(N < M)
+            return -1;
+
+        if(S > 6 && 6 * N < 7 * M)
+            return -1;
+            
+        int sunday = S/7;
+        int totalDays = S-sunday;
+        int ans = 0;
+        int totalFood = S*M;
+        
+        if(totalFood % N == 0){
+            ans = totalFood/N;
+        }else{
+            ans = totalFood/N + 1;
+        }
+        
+         if(ans <= totalDays)
+            return ans;
+        return -1;
+    }
+};
+
+class Mincost {
+  public:
+    int minCost(vector<int>& arr) {
+        // code here
+         priority_queue<int, vector<int>, greater<int>> pq;
+         for(int i =0;i<arr.size();i++){
+             pq.push(arr[i]);
+         }
+         int Mincost = 0;
+         while(pq.size()>1){
+             int first = pq.top();
+             pq.pop();
+             int second = pq.top();
+             pq.pop();
+             int cost = first + second;
+             Mincost += cost;
+             pq.push(cost);
+         }
+         
+         return Mincost;
+    }
+};
+
+struct item{
+  public:
+  int val;
+  int wt;
+  double ratio;
+  item(int v, int w , double r){
+      val = v;
+      wt = w;
+      ratio = r;
+  }
+};
+class cmp{
+  public:
+  bool operator()(item a, item b){
+      return a.ratio > b.ratio;
+  }
+};
+class fractionalKnapstack {
+  public:
+    double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
+        // code here
+        vector<item> v;
+        for(int i = 0;i<wt.size();i++){
+            int value = val[i];
+            int w = wt[i];
+            item temp(value,w, (double)value/w);
+            v.push_back(temp);
+        }
+        
+        sort(v.begin(),v.end(),cmp());
+        double ans = 0;
+        int currweight = 0;
+        for(int i =0;i<val.size();i++){
+            if(currweight + v[i].wt <= capacity){
+                currweight += v[i].wt;
+                ans += v[i].val;
+            }else{
+                int remain = capacity -currweight;
+                ans += remain * v[i].ratio;
+                break;
+                
+            }
+        }
+        
+        return ans;
+        
+    }
+};
+
 
 int main() {
     
